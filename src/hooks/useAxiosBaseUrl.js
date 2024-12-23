9e72d92a67c84985e46800fef9c5a1b684eee608
shorif2 <shorifahamed571@gmail.com>
2024-12-24 01:23:37 +0600

    error fixing

--- src/hooks/useAxiosBaseUrl.js
+++ src/hooks/useAxiosBaseUrl.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const useAxiosBaseUrl = axios.create({
-  // baseURL: "http://localhost:5000",
-  baseURL: "https://gift-haven-client-two.vercel.app",
+  baseURL: "http://localhost:5000",
+  // baseURL: "https://gift-haven-client-two.vercel.app",
 });
 
 export default useAxiosBaseUrl;
